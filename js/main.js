15e0dbcb1f014614b6b8ff5fab45b69b7c70c1c9
Nisha Singhh <nishasingh20031998@gmai.com>
2026-06-10 00:34:19 +0530

    Initial portfolio website commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+
+    sections.forEach(section => {
+
+        const sectionTop = section.getBoundingClientRect().top;
+
+        if (sectionTop < window.innerHeight - 100) {
+            section.classList.add("show");
+        }
+
+    });
+
+});
+
+document.querySelector("#home").classList.add("show");
